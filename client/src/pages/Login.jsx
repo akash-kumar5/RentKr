5d3ca330bf1dffdd025d69779773a3838bd23b5b
Akash Kumar <akashsingh4152@gmail.com>
2025-07-18 18:52:13 +0530

    fixing links

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("/login", {
+      const response = await fetch("https://rentkr.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
